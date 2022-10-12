7a0a68e5d7408740a7a1ee4ef135e5ac2665d39a
Victor Raul Huaman Simeon <victorhuamansimeon@gmail.com>
2022-10-12 09:08:39 -0500

    Pregunta02-Listo por Huaman

---
+++ Preg02.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+int main()
+{
+	int n,a;
+	cout<<"Ingrese un numero : "; cin >>n;
+	
+	a=(((n)*(n+1))/2);
+	
+	cout <<"La suma de los " << n << " numeros es : "<<endl;
+	cout<< a;
+	
+	return 0;
+	
+}
